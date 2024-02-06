ec7a129e7617dc128ca933d6747f4d3f1fc4d52b
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-02-07 00:11:26 +0530

    Update Remove.py

--- AnonXMusic/plugins/admins/Remove.py
+++ AnonXMusic/plugins/admins/Remove.py
@@ -103,6 +103,8 @@ async def rm_song(client, CallbackQuery, _):
                 user = check[0]["by"]
                 streamtype = check[0]["streamtype"]
                 videoid = check[0]["vidid"]
+                userinfo = check[0]["userinfo"]
+                musicid = check[0]["musicid"]
                 status = True if str(streamtype) == "video" else None
                 db[chat_id][0]["played"] = 0
                 exis = (check[0]).get("old_dur")
@@ -123,7 +125,7 @@ async def rm_song(client, CallbackQuery, _):
                                                 await Anony.skip_stream(chat_id, link, video=status, image=image)
                                 except:
                                                 return await message.reply_text(_["call_6"])
-                                button = stream_markup(_, chat_id)
+                                button = stream_markup(_, musicid, userinfo, chat_id)
                                 img = await get_thumb(videoid)
                                 run = await message.reply_photo(
                                                 photo=img,
@@ -156,7 +158,7 @@ async def rm_song(client, CallbackQuery, _):
                                                 await Anony.skip_stream(chat_id, file_path, video=status, image=image)
                                 except:
                                                 return await mystic.edit_text(_["call_6"])
-                                button = stream_markup(_, chat_id)
+                                button = stream_markup(_, musicid, userinfo, chat_id)
                                 img = await get_thumb(videoid)
                                 run = await message.reply_photo(
                                                 photo=img,
@@ -176,7 +178,7 @@ async def rm_song(client, CallbackQuery, _):
                                                 await Anony.skip_stream(chat_id, videoid, video=status)
                                 except:
                                                 return await message.reply_text(_["call_6"])
-                                button = stream_markup(_, chat_id)
+                                button = stream_markup(_, musicid, userinfo, chat_id)
                                 run = await message.reply_photo(
                                                 photo=config.STREAM_IMG_URL,
                                                 caption=_["stream_2"].format(user),
@@ -199,7 +201,7 @@ async def rm_song(client, CallbackQuery, _):
                                 except:
                                                 return await message.reply_text(_["call_6"])
                                 if videoid == "telegram":
-                                                button = stream_markup(_, chat_id)
+                                                button = stream_markup(_, musicid, userinfo, chat_id)
                                                 run = await message.reply_photo(
                                                                 photo=config.TELEGRAM_AUDIO_URL
                                                                 if str(streamtype) == "audio"
@@ -212,7 +214,7 @@ async def rm_song(client, CallbackQuery, _):
                                                 db[chat_id][0]["mystic"] = run
                                                 db[chat_id][0]["markup"] = "tg"
                                 elif videoid == "soundcloud":
-                                                button = stream_markup(_, chat_id)
+                                                button = stream_markup(_, musicid, userinfo, chat_id)
                                                 run = await message.reply_photo(
                                                                 photo=config.SOUNCLOUD_IMG_URL
                                                                 if str(streamtype) == "audio"
@@ -225,7 +227,7 @@ async def rm_song(client, CallbackQuery, _):
                                                 db[chat_id][0]["mystic"] = run
                                                 db[chat_id][0]["markup"] = "tg"
                                 else:
-                                                button = stream_markup(_, chat_id)
+                                                button = stream_markup(_, musicid, userinfo, chat_id)
                                                 img = await get_thumb(videoid)
                                                 run = await message.reply_photo(
                                                                 photo=img,
