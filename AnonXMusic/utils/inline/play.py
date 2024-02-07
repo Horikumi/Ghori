c1666241391f58d3b1d8dad80891f6c3dc9a9e5e
Horikumi <148572875+Horikumi@users.noreply.github.com>
2024-02-07 19:14:42 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -1,7 +1,6 @@
 import math
 
 from pyrogram.types import InlineKeyboardButton
-
 from AnonXMusic.utils.formatters import time_to_seconds
 
 
@@ -27,7 +26,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
     return buttons
 
 
-def stream_markup_timer(_, musicid, user_id, chat_id, played, dur):
+def stream_markup_timer(_, musicid, user_id, chat_id, played, dur):  
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
@@ -54,17 +53,17 @@ def stream_markup_timer(_, musicid, user_id, chat_id, played, dur):
         bar = "━━━━━━━━━⦿"
     buttons = [
         [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
+        [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
         ],
         [InlineKeyboardButton(text=_["remove_11"], callback_data=f"ClearSong {musicid}|{user_id}|{chat_id}")],
     ]
