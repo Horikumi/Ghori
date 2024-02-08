b8172b96e1cb72edef84430044f5fdf374b1db1a
Horikumi <148572875+Horikumi@users.noreply.github.com>
2024-02-08 12:47:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Horikumi/Anon",
+    "https://github.com/Horikumi/Ghori",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
