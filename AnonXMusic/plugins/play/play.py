257af393e3bd4c2b95b9f6c697ecfb95eb4b74da
Horikumi <148572875+Horikumi@users.noreply.github.com>
2024-02-16 15:04:21 +0530

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusic.utils.decorators.database import is_video_allowed
+from AnonXMusic.utils.database import is_video_allowed
 from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
 from AnonXMusic.core.call import Anony
 from AnonXMusic.utils import seconds_to_min, time_to_seconds
