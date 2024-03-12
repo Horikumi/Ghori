448346b262f659f90e3130fe5fc37d3f6093cc8a
Horikumi <148572875+Horikumi@users.noreply.github.com>
2024-03-13 01:22:21 +0530

    Update call.py

--- AnonXMusic/core/call.py
+++ AnonXMusic/core/call.py
@@ -46,15 +46,16 @@ async def _clear_(chat_id):
     try:
        check = db.get(chat_id)
        if check:
-            position = len(check) - 1
-            popped = check.pop(position)
-            if popped:
-               await auto_clean(popped)
+            for position in range(len(check) - 1, -1, -1):
+                popped = check.pop(position)     
+                if popped:
+                    await auto_clean(popped)
     except:
       pass
     db[chat_id] = []
     await remove_active_video_chat(chat_id)
     await remove_active_chat(chat_id)
+
 
 
 class Call(PyTgCalls):
